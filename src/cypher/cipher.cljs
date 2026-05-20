c7b4c49aaf466f591bd9b08f0557dd6c4ec00f9a
murilinhops <murilinhorps@gmail.com>
2026-05-19 22:35:38 -0300

    .

--- src/cypher/cipher.cljs
+++ src/cypher/cipher.cljs
@@ -2,3 +2,6 @@
   (:require [clojure.string :as str]))
 
 (def ^:private alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(defn build-pair-map [keyword] 
+  (let [keyword-chars (->> keyword str/lower-case distinct)]))
