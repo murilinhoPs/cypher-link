1c5004699c87355aacd5e8d18e809f60f0caa22e
murilinhops <murilinhorps@gmail.com>
2026-05-19 22:27:59 -0300

    remove code

--- src/cypher/cipher.cljs
+++ src/cypher/cipher.cljs
@@ -2,24 +2,3 @@
   (:require [clojure.string :as str]))
 
 (def ^:private alphabet "abcdefghijklmnopqrstuvwxyz")
-
-(defn build-pair-map [kw]
-  (let [kw-chars (->> kw str/lower-case distinct)]
-    (reduce
-      (fn [m ch]
-        (let [idx (str/index-of alphabet (str ch))]
-          (if (or (nil? idx) (contains? m ch))
-            m
-            (let [partner (get alphabet (if (= idx 25) 24 (inc idx)))]
-              (if (contains? m partner)
-                m
-                (assoc m ch partner partner ch))))))
-      {}
-      kw-chars)))
-
-(defn cipher [text kw]
-  (let [pair-map (build-pair-map kw)]
-    (->> text
-         str/lower-case
-         (map #(get pair-map % %))
-         (apply str))))
